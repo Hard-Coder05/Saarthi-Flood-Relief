fd2595c7d786d1823a359e7c768de5622ccfc4f4
sahil9001 <sahilsilare@gmail.com>
2022-01-22 11:12:46 +0530

    feat: Added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../src/routes/HomePage/Home";
+import NavBar from "../src/components/NavBar";
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
