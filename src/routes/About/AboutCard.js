aaae9681ba94051a33758bb670ab8e119031af9d
Prakhar Asaiya <63089922+prakhar-asaiya@users.noreply.github.com>
2022-01-22 23:35:03 +0530

    Completed AboutCard and About Us

--- src/routes/About/AboutCard.js
+++ src/routes/About/AboutCard.js
@@ -2,6 +2,23 @@ import React, { Component } from "react";
 
 export default class AboutCard extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div className="flex flex-col">
+          <img
+            src={this.props.url}
+            alt="flood"
+            style={{ height: "600px", width: "full" }}
+            className="object-cover rounded-3xl"
+          />
+          <p className="text-2xl font-medium font-inter text-center py-2 tracking-wide">
+            {this.props.name}
+          </p>
+          <p className="text-lg font-regular font-inter text-center tracking-wide">
+            {this.props.occupation}
+          </p>
+        </div>
+      </div>
+    );
   }
 }
